7e676317144e0a06f06ed38c5f35b7e5481164c9
Philip Scott <pscott@zeptohost.com>
2019-09-02 19:15:21 -0400

    use hash router

--- client-react/src/containers/router.tsx
+++ client-react/src/containers/router.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { Lander } from './lander'
 import { Room } from './room'
 
